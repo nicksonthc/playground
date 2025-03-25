0250e1b1fb7e583d07a7fc61fe97e232cf5682a0
Nickson Tan Hoo Chuan <nicksonthc@gmail.com>
2025-03-25 23:12:05 +0800

    fix: routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import Pathfinding from "./Pathfinding";
 import GameTheory from "./GameTheory";
 import ProjectCalculator from "./ProjectCalculator";
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Router basename="/playground">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pathfinding" element={<Pathfinding />} />
-          <Route path="/game-theory" element={<GameTheory />} />
-          <Route path="/project-calculator" element={<ProjectCalculator />} />
-        </Routes>
-      </Router>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pathfinding" element={<Pathfinding />} />
+        <Route path="/game-theory" element={<GameTheory />} />
+        <Route path="/project-calculator" element={<ProjectCalculator />} />
+      </Routes>
     </div>
   );
 }
